faa783d358daf9e1156ca742eff9b3d03c04f28b
Michal Swietek <michal.swietek@cardinals.cc>
2021-11-23 22:13:22 +0100

    simple tests

--- 18.11/crypto/src/diffie_hellman.rs
+++ 18.11/crypto/src/diffie_hellman.rs
@@ -29,10 +29,36 @@ impl EncryptionKey {
     }
 
     pub fn is_correct(&self, _epk1: &EncryptionPublicKey, _ek2: &EncryptionPublicKey) -> bool {
-        true
+        false
     }
 }
 
 pub fn hash(x: &[u8]) -> [u8; 32] {
     Sha3_256::digest(x).into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encrypt_decrypt() {
+        let enckey = EncryptionKey {};
+        let scalar = Scalar::from_raw([1729, 2137, 1994, 1500]);
+        let enc = enckey.encrypt(&scalar);
+        let dec = enckey.decrypt(&enc);
+
+        assert!(dec.is_some() && dec.unwrap() == scalar);
+    }
+
+    #[test]
+    fn correctness() {
+        let s1 = Scalar::one();
+        let s2 = Scalar::one().double();
+        let epk1 = EncryptionPublicKey::from_scalar(s1);
+        let epk2 = EncryptionPublicKey::from_scalar(s2);
+        let enckey = epk1.to_encryption_key(s2);
+
+        assert!(enckey.is_correct(&epk1, &epk2));
+    }
 }
